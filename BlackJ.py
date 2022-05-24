407468be9e34b6e6015aaf6355263c469433f971
Ondřej Tulach <ondrej.tulach@gmail.com>
2022-05-24 21:21:29 +0200

    Casino taky hraje (trochu).

--- BlackJ.py
+++ BlackJ.py
@@ -1,9 +1,29 @@
 from operator import ne
 import random
 
-def dalsikarta(karty, hodnota):
+def casino(karty, hodnotacasina):
+    if hodnotacasina >= 16:
+        print("casino má", hodnotacasina)
+
+    else:
+        a = random.choice(karty)
+    hodnotacasina += a
+    if hodnotacasina > 21:
+        print("casino má", hodnotacasina)
+        print("je přes!, vyhrál jsi")
+        exit()
+    if hodnotacasina <= 21:
+        print("casino má", hodnotacasina)
+
+    return hodnotacasina
+    
+
+
+
+def dalsikarta(karty, hodnota, hodnotacasina):
     a = random.choice(karty)
     hodnota += a
+    hodnotacasina = casino(karty, hodnotacasina)
     if hodnota > 21:
         print("dostal jsi ", a)
         print("celkem máš ", hodnota)
@@ -15,30 +35,35 @@ def dalsikarta(karty, hodnota):
     if hodnota == 21:
         print("dostal jsi ", a)
         print("celkem máš ", hodnota)
-        print("vyhrál jsi")
-        exit()
-    return hodnota
+        if hodnota > hodnotacasina:
+            print("vyhrál jsi")
+            exit()
+        else:
+            print("remíza")   
+            exit()
+    return hodnota, hodnotacasina
 
-def porovnej(hodnota):
-    if hodnota > 16:
+
+def porovnej(hodnota, hodnotacasina):
+    if hodnota > hodnotacasina:
         print("vyhrál jsi")
-    if hodnota == 16:
+    if hodnota == hodnotacasina:
         print("remíza")
-    if hodnota < 16:
+    if hodnota < hodnotacasina:
         print("prohrál jsi")   
 
-def cobudedal(karty, hodnota):
+def cobudedal(karty, hodnota, hodnotacasina):
     print("chceš další?")
     a = input()
     if a == "ne":
         print("celkem máš ", hodnota)
-        print("dealer má 16")
-        porovnej(hodnota)
+        print("casino má", hodnotacasina)
+        porovnej(hodnota, hodnotacasina)
     if a == "ano":
-        hodnota = dalsikarta(karty, hodnota)
-        cobudedal(karty, hodnota)
+        (hodnota, hodnotacasina) = dalsikarta(karty, hodnota, hodnotacasina)
+        cobudedal(karty, hodnota, hodnotacasina)
 
 karty = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
 
-prvni = dalsikarta(karty, 0)
-cobudedal(karty, prvni)
+(prvni, prvnicasina) = dalsikarta(karty, 0, 0)
+cobudedal(karty, prvni, prvnicasina)
